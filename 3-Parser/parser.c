f86d40185f00a6a4cdc8628c76ad3f8ac58dbd48
Maahee <58122827+msabur@users.noreply.github.com>
2021-07-06 18:59:16 -0400

    Set up error handling

--- 3-Parser/parser.c
+++ 3-Parser/parser.c
@@ -1,15 +1,21 @@
 /*
-	Author: Noelle Midkiff
+Author: Noelle Midkiff
 */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <setjmp.h>
 #include "compiler.h"
+
+/* Set up error management */
+int error;
+jmp_buf env;
+#define catch() setjmp(env)
+#define throw(value) longjmp(env, error = value)
 
 symbol *table;
 int sym_index;
-int error;
 
 void printtable();
 void errorend(int x);
@@ -19,9 +25,10 @@ symbol *parse(lexeme *input)
 	table = malloc(1000 * sizeof(symbol));
 	sym_index = 0;
 	error = 0;
-	
-	if (error)
+
+	if (catch() != 0)
 	{
+		errorend(error);
 		free(table);
 		return NULL;
 	}
@@ -36,53 +43,53 @@ void errorend(int x)
 {
 	switch (x)
 	{
-		case 1:
-			printf("Parser Error: Competing Symbol Declarations\n");
-			break;
-		case 2:
-			printf("Parser Error: Unrecognized Statement Form\n");
-			break;
-		case 3:
-			printf("Parser Error: Programs Must Close with a Period\n");
-			break;
-		case 4:
-			printf("Parser Error: Symbols Must Be Declared with an Identifier\n");
-			break;
-		case 5:
-			printf("Parser Error: Constants Must Be Assigned a Value at Declaration\n");
-			break;
-		case 6:
-			printf("Parser Error: Symbol Declarations Must Be Followed By a Semicolon\n");
-			break;
-		case 7:
-			printf("Parser Error: Undeclared Symbol\n");
-			break;
-		case 8:
-			printf("Parser Error: while Must Be Followed By do\n");
-			break;
-		case 9:
-			printf("Parser Error: if Must Be Followed By then\n");
-			break;
-		case 10:
-			printf("Parser Error: begin Must Be Followed By end\n");
-			break;
-		case 11:
-			printf("Parser Error: while and if Statements Must Contain Conditions\n");
-			break;
-		case 12:
-			printf("Parser Error: Conditions Must Contain a Relational-Operator\n");
-			break;
-		case 13:
-			printf("Parser Error: ( Must Be Followed By )\n");
-			break;
-		case 14:
-			printf("Parser Error: call and read Must Be Followed By an Identifier\n");
-			break;
-		default:
-			printf("Implementation Error: Unrecognized Error Code\n");
-			break;
+	case 1:
+		printf("Parser Error: Competing Symbol Declarations\n");
+		break;
+	case 2:
+		printf("Parser Error: Unrecognized Statement Form\n");
+		break;
+	case 3:
+		printf("Parser Error: Programs Must Close with a Period\n");
+		break;
+	case 4:
+		printf("Parser Error: Symbols Must Be Declared with an Identifier\n");
+		break;
+	case 5:
+		printf("Parser Error: Constants Must Be Assigned a Value at Declaration\n");
+		break;
+	case 6:
+		printf("Parser Error: Symbol Declarations Must Be Followed By a Semicolon\n");
+		break;
+	case 7:
+		printf("Parser Error: Undeclared Symbol\n");
+		break;
+	case 8:
+		printf("Parser Error: while Must Be Followed By do\n");
+		break;
+	case 9:
+		printf("Parser Error: if Must Be Followed By then\n");
+		break;
+	case 10:
+		printf("Parser Error: begin Must Be Followed By end\n");
+		break;
+	case 11:
+		printf("Parser Error: while and if Statements Must Contain Conditions\n");
+		break;
+	case 12:
+		printf("Parser Error: Conditions Must Contain a Relational-Operator\n");
+		break;
+	case 13:
+		printf("Parser Error: ( Must Be Followed By )\n");
+		break;
+	case 14:
+		printf("Parser Error: call and read Must Be Followed By an Identifier\n");
+		break;
+	default:
+		printf("Implementation Error: Unrecognized Error Code\n");
+		break;
 	}
-	
+
 }
 
 void printtable()
