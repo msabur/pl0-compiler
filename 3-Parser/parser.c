a7f68fd9a2a34c910e1f006a026e3f9a6f407d9d
Grant Allan <45442774+Grant-Allan@users.noreply.github.com>
2021-07-14 16:07:47 -0400

    Commented parser.c
    So, so, so many comments. Plus a few minor changes to the code for readability's sake.

--- 3-Parser/parser.c
+++ 3-Parser/parser.c
@@ -13,11 +13,19 @@
 #include <setjmp.h>
 #include "compiler.h"
 
-/* Constants */
-// Symbol types
-const int constkind = 1, varkind = 2, prockind = 3;
-// Options for symbol table lookups 
-const int o_const = 1<<1, o_var = 1<<2, o_proc = 1<<3;
+enum
+{
+	constkind = 1,
+	varkind = 2,
+	prockind = 3
+};
+
+enum
+{
+	op_const = 1 << 1,
+	op_var = 1 << 2,
+	op_proc = 1 << 3
+};
 
 /* Error management */
 int error;
@@ -27,21 +35,26 @@ jmp_buf env;
 
 symbol *table;
 int sym_index;
+
+// Token is the current token
+// List is the global we'll use to move through the input
 lexeme token, *list;
-int curLevel; // tracks our lexicographical level
+
+// Tracks our lexicographical level
+int level;
 
 /* Utilities */
 void printtable();
 void printErrorMessage(int x);
 void getToken();
 void expect(int token_type, int err);
-void addSymbol(char *name, int val, int kind, int addr);
+void addSymbol(char *name, int val, int type);
 symbol *fetchSymbol(char *name, int kinds);
 void markSymbolsInScope();
 bool conflictingSymbol(char *name, int kind);
 
-/* Parsing functions */
-void program(); // starting symbol
+/* Parsing Functions */
+void program();
 void block();
 void const_declaration();
 void var_declaration();
@@ -52,6 +65,7 @@ void expression();
 void term();
 void factor();
 
+/* Main Function */
 symbol *parse(lexeme *input)
 {
 	// Initializing symbol table and token list
@@ -67,62 +81,133 @@ symbol *parse(lexeme *input)
 	}
 	else
 	{
-		program();	  // We begin parsing!
-		printtable(); // Print the symbol table
+		// We begin parsing!
+		program();
+		
+		// Print the symbol table
+		printtable();
+
 		return table;
 	}
 }
 
+/* Start the program */
 void program()
 {
-	// the main procedure is always the first item in our symbol table
-	addSymbol("main", 0, prockind, 0);
+	// Load main into the symbol table
+	addSymbol("main", 0, procsym);
+
+	// Get the first token of the program
 	getToken();
+
+	// This will be the main block. That is, the entire program.
 	block();
+
+	// The program must end with a period
 	expect(periodsym, 3);
 }
 
+/* Start other functions */
 void block()
 {
 	if (token.type == constsym)
 		const_declaration();
+	
 	if (token.type == varsym)
 		var_declaration();
-	while (token.type == procsym)
+	
+	if (token.type == procsym)
 		procedure_declaration();
 
 	statement();
 }
 
+/* Process a constant */
 void const_declaration()
 {
-	// Find the name of the constant
+	// Use ident to hold the name and value of the constant
 	lexeme ident;
-	getToken();
-	expect(identsym, 4);
-	ident = token;
-	
-	// Check if adding it to the symbol table will cause a conflict
-	if (conflictingSymbol(ident.name, constkind))
-		throw(1);
 
-	// Find the value of the constant
+	// This token should have the constant name in it
 	getToken();
+
+	// If there's no name, throw an error
+	// Symbols Must Be Declared with an Identifier
+	expect(identsym, 4);
+
+	// Load the constant name into the ident
+	ident = token;
+
+	// This token should have the := in it
+	getToken();
+
+	// Equals (":="")
+	// Constants Must Be Assigned a Value at Declaration
 	expect(becomessym, 5);
+
+	// This token should have the value of the constant in it
 	getToken();
+
+	// Make sure there's a number it's being set equal to
+	// Constants Must Be Assigned a Value at Declaration
 	expect(numbersym, 5);
+
+	// Load the constant value into the ident
 	ident.value = token.value;
 
+	// Check for conflicting symbols
+	// Parser Error: Competing Symbol Declarations
+	if (conflictingSymbol(ident.name, 1))
+		throw(1);
 	
-	addSymbol(ident.name, ident.value, constkind, 0);
+	// Add the constant to the symbol table
+	addSymbol(ident.name, ident.value, constsym);
 
+	// Get the next token
 	getToken();
 
 	// If we see a comma, we have to parse another declaration
 	if (token.type == commasym)
-	{
 		const_declaration();
+	// Otherwise we need semicolon to mark the end of the declaration
+	else
+	{
+		// Symbol Declarations Must Be Followed By a Semicolon
+		expect(semicolonsym, 6);
+
+		// Get the next token
+		getToken();
 	}
+}
+
+/* Process a constant */
+void var_declaration()
+{
+	// Use ident to hold the name and value of the variable
+	lexeme ident;
+
+	// This token should hold the name of the variable
+	getToken();
+
+	// Symbols Must Be Declared with an Identifier
+	expect(identsym, 4);
+
+	// Fill ident with the variable's token information
+	ident = token;
+
+	// We add it to the symbol table only if it wasn't already there
+	if (conflictingSymbol(ident.name, 2))
+		throw(1);
+
+	// Add the variable to the symbol table	
+	addSymbol(ident.name, 0, varsym);
+
+	// Get the next token to check it
+	getToken();
+
+	// If we see a comma, we have to parse another declaration
+	if (token.type == commasym)
+		var_declaration();
 	// Otherwise we need semicolon to mark the end of the declaration
 	else
 	{
@@ -131,51 +216,39 @@ void const_declaration()
 	}
 }
 
-void var_declaration()
-{
-	int numVars = 0;
-	do {
-		// Find the name of the variable
-		lexeme ident;
-		getToken();
-		expect(identsym, 4);
-		ident = token;
-
-		// Check if adding it to the symbol table will cause a conflict
-		if (conflictingSymbol(ident.name, varkind))
-			throw(1);
-		addSymbol(ident.name, 0, varkind, numVars + 3);
-		numVars++;
-
-		getToken();
-
-		// If we see a comma, we have to parse another declaration
-	} while (token.type == commasym);
-
-	// Otherwise we need semicolon to mark the end of the declaration
-	expect(semicolonsym, 6); // need ; at end of declaration
-	getToken();
-}
-
+/* Process a procedure */
 void procedure_declaration()
 {
-	// Find the name of the procedure
+	// Use ident to hold the name and value of the procedure
 	lexeme ident;
+
+	// This token should hold the name of the procedure
 	getToken();
+
+	// Symbols Must Be Declared with an Identifier
 	expect(identsym, 4);
+
+	// Fill ident with the procedure's information
 	ident = token;
 
-	// Check if adding it to the symbol table will cause a conflict
-	if (conflictingSymbol(ident.name, prockind))
+	// We add it to the symbol table only if it wasn't already there
+	if (conflictingSymbol(ident.name, 3))
 		throw(1);
-	addSymbol(ident.name, 0, prockind, 0);
+	
+	// Add the procedure to the symbol table
+	addSymbol(ident.name, 0, procsym);
 
-	getToken();
-	expect(semicolonsym, 6); // need ';' at end of declaration
+	// This token should have a semicolon in it
 	getToken();
 
-	// A procedure's body is its own scope. We track that with curLevel
-	curLevel++;
+	// Symbol Declarations Must Be Followed By a Semicolon
+	expect(semicolonsym, 6);
+
+	// Grab the next token so the block can process properly
+	getToken();
+
+	// Increase the lexicographical level for everything inside the procedure.
+	level++;
 
 	// Parse the body of the procedure
 	block();
@@ -183,12 +256,18 @@ void procedure_declaration()
 	// Make sure the procedure's local variables can't be used outside
 	markSymbolsInScope();
 
-	curLevel--;
+	// Decrease level now that we're outside the procedure
+	level--;
 
+	// Check to make sure the whole thing ends with a ";"
+	// Symbol Declarations Must Be Followed By a Semicolon
 	expect(semicolonsym, 6);
+
+	// Grab the next token for processing
 	getToken();
 }
 
+/* Process a statement */
 void statement()
 {
 	symbol *sym;
@@ -196,112 +275,219 @@ void statement()
 	{
 	case identsym:
 		// Make sure that we only assign to variables
-		sym = fetchSymbol(token.name, o_var);
+		sym = fetchSymbol(token.name, op_var);
+
+		// If it's not a variables, throw an error
+		// Parser Error: Undeclared Symbol
 		if (!sym)
 			throw(7);
+		
+		// This should hold :=
 		getToken();
+
+		// Unrecognized Statement Form
 		expect(becomessym, 2);
+		
+		// This should hold the first item in the expression
 		getToken();
+		
+		// Process the expression
 		expression();
+
+		// Parser Error: Unrecognized Statement Form
 		if (token.type == lparentsym)
 			throw(2);
+
 		break;
+	
 	case callsym:
+		// This should hold the name of a procedure
 		getToken();
+
+		// call and read Must Be Followed By an Identifien
 		expect(identsym, 14);
+		
 		// Make sure that we only call procedures
-		sym = fetchSymbol(token.name, o_proc);
+		sym = fetchSymbol(token.name, op_proc);
+
+		// If it's not a procedure, throw an error
+		// Parser Error: Undeclared Symbol
 		if (!sym)
 			throw(7);
+		
+		// Move onto the next token to be processed
 		getToken();
 		break;
+	
 	case beginsym:
 		do
 		{
+			// Get the first token of the statement; should be
+			// the variable whose value is being changed.
 			getToken();
+
+			// Process the statement
 			statement();
+
+		// Process all statements in the procedure
 		} while (token.type == semicolonsym);
+
+		// Make sure the procedure ends with end
+		// begin Must Be Followed By end
 		expect(endsym, 10);
+
+		// Move onto the next token to be processed
 		getToken();
 		break;
+	
 	case ifsym:
+		// Get the first token of the condition to be processed
 		getToken();
+
+		// Process the condition
 		condition();
+
+		// Make sure the next symbol is then
+		// if Must Be Followed By then
 		expect(thensym, 9);
+
+		// Get the first symbol of the statement inside the if
 		getToken();
+
+		// Process the statement
 		statement();
+
+		// If there's an else...
 		if (token.type == elsesym)
 		{
+			// Get the first token of the statement
 			getToken();
+
+			// Process the statement
 			statement();
 		}
 		break;
+	
 	case whilesym:
+		// Get the first token of the condition
 		getToken();
+
+		// Process the condition
 		condition();
+
+		// Make sure the next symbol is do
+		// while Must Be Followed By do
 		expect(dosym, 8);
+
+		// Get the first symbol of the statement inside the while
 		getToken();
+
+		// Process the statement
 		statement();
 		break;
+	
 	case readsym:
+		// This should have an identity in it
 		getToken();
+
+		// call and read Must Be Followed By an Identifien
 		expect(identsym, 14);
+
 		// Make sure to only read input into variables
-		sym = fetchSymbol(token.name, o_var);
+		sym = fetchSymbol(token.name, op_var);
+		
+		// Make sure we're reading a variable
+		// Parser Error: Undeclared Symbol
 		if (!sym)
 			throw(7);
+		
+		// Get the next token to be processed
 		getToken();
 		break;
+	
 	case writesym:
+		// This should have an identity in it
 		getToken();
+
+		// Parser Error: Unrecognized Statement Form
 		expect(identsym, 2);
+		
 		// Make sure to only write constants or variables to the screen
-		sym = fetchSymbol(token.name, o_const | o_var);
+		sym = fetchSymbol(token.name, op_const | op_var);
+
+		// Make sure we're writing a constant or a variable
+		// Parser Error: Undeclared Symbol
 		if (!sym)
 			throw(7);
+
+		// Get the next token to be processed
 		getToken();
 		break;
+	
 	default:
 		break;
 	}
 }
 
+/* Process a formula */
 void condition()
 {
 	// We should throw an error when a condition is missing
 	bool missing_condition = true;
 
+	// Symbosl that can be the start of a condition
+	token_type first[] = {oddsym, plussym, minussym, identsym, numbersym, lparentsym};
+
 	// Checking if the current token can be the start of a condition
-	token_type first[] = {oddsym, plussym, minussym, identsym,
-						  numbersym, lparentsym};
 	for (int i = 0; i < sizeof(first) / sizeof(*first); i++)
 		if (token.type == first[i])
 			missing_condition = false;
+	
+	// If not, then throw an error
 	if (missing_condition)
 		throw(11);
 
 	if (token.type == oddsym)
 	{
+		// Get the first token of the expression
 		getToken();
+
+		// Process the expression
 		expression();
 	}
 	else
 	{
+		// Process the expression
 		expression();
+
 		// Here we throw an error if there is no relational operator.
 		// The relational operators are from eqlsym to geqsym
+		// Frankly, this is overkill, since the previous checker will have
+		// caught any invalid symbols.
+		// Conditions Must Contain a Relational-Operator
 		if (token.type < eqlsym || token.type > geqsym)
 			throw(12);
+		
+		// Get the first token of the expression
 		getToken();
+
+		// Process the expression
 		expression();
 	}
 }
 
+/* Process Plus and/or Minus */
 void expression()
 {
+	// If it's a plus or minus, move to the next token
 	if (token.type == plussym || token.type == minussym)
 		getToken();
+	
+	// Process the term
 	term();
+
+	// As long as there's another plus or minus, keep processing
+	// the expression
 	while (token.type == plussym || token.type == minussym)
 	{
 		getToken();
@@ -309,9 +495,13 @@ void expression()
 	}
 }
 
+/* Process multiplication, division, and/or modulus */
 void term()
 {
+	// Process the factor
 	factor();
+
+	// As long as the current token is * / or %, keep processing
 	while (token.type == multsym || token.type == slashsym || token.type == modsym)
 	{
 		getToken();
@@ -319,88 +509,127 @@ void term()
 	}
 }
 
+/* Process the factor */
 void factor()
 {
 	if (token.type == identsym)
 	{
 		// Only consts and vars are allowed in arithmetic expressions
-		symbol *sym = fetchSymbol(token.name, o_const | o_var);
+		symbol *sym = fetchSymbol(token.name, op_const | op_var);
+
+		// If it's not a constant or a variable, throw an error
+		// Parser Error: Undeclared Symbol
 		if (!sym)
 			throw(7);
+
+		// Get the next token to be processed
 		getToken();
 	}
 	else if (token.type == numbersym)
 	{
+		// Get the next token to be processed
 		getToken();
 	}
 	else if (token.type == lparentsym)
 	{
+		// Get the first token of the expression
 		getToken();
+
+		// Process the expression
 		expression();
+
+		// Make sure to close the parentheses
+		// ( Must Be Followed By )
 		expect(rparentsym, 13);
+
+		// Get the next token to be processed
 		getToken();
 	}
 	else
 	{
-		// We shouldn't reach here
-		throw(11);
+		/*
+		 * Not sure whether to throw an error here.
+		 * The recitation slide throws 'identifier, (, or number expected',
+		 * but that doesn't match any of the errors in the instructions.
+		 */
+		fprintf(stderr, "Here we are!");
 	}
 }
 
-// Throws an error if the current token isn't of the expected type
+/* Throws an error if the current token isn't of the expected type */
 void expect(int token_type, int err)
 {
 	if (token.type != token_type)
 		throw(err);
 }
 
-// Fetches the next token and assigns it to the global variable token
+/* Fetches the next token and assigns it to the global variable token */
 void getToken()
 {
 	static int tokenIndex = 0;
 	token = list[tokenIndex++];
 }
 
-// Adds a symbol to the symbol table
-void addSymbol(char *name, int val, int kind, int addr)
+/* Adds a symbol to the symbol table */
+void addSymbol(char *name, int val, int type)
 {
+	int kind, address;
+
+	// Determine kind
+	if (type == constsym)
+		kind = constkind;
+	else if (type == varsym)
+		kind = varkind;
+	else
+		kind = prockind;
+
+	// Determine address
+	if (kind == prockind || kind == constkind)
+		address = 0;
+	else if (table[sym_index - 1].addr == 0)
+		address = 3; // first symbol has offset 3
+	else
+		address = table[sym_index - 1].addr + 1; // for normal variables
+
 	// Now, we add this symbol to the table
-	table[sym_index].addr = addr;
+	table[sym_index].addr = address;
 	table[sym_index].kind = kind;
 	table[sym_index].val = val;
 	table[sym_index].mark = 0;
-	table[sym_index].level = curLevel;
+	table[sym_index].level = level;
 	strcpy(table[sym_index].name, name);
 
 	// Increment the index
-	++sym_index;
+	sym_index++;
 }
 
-// Returns the requested symbol, or NULL if it's not found.
-// The options parameter tells the kind of symbol to fetch.
-// If multiple results are found, the first one is returned.
+/* Returns the requested symbol, or NULL if it's not found.
+ * The options parameter tells the kind of symbol to fetch.
+ * If multiple results are found, the first one is returned.
+ */
 symbol *fetchSymbol(char *name, int kinds)
 {
 	for (int i = sym_index - 1; i != -1; i--)
 	{
-		// ignore marked symbols
+		// Ignore marked symbols
 		if (table[i].mark)
 			continue;
-		// ignore symbols whose names don't match what we want
+		
+		// Ignore symbols whose names don't match what we want
 		if (strcmp(table[i].name, name) != 0)
 			continue;
-		// if the current symbol is of a requested kind, return it
+		
 		if (kinds & (1 << table[i].kind))
 			return &table[i];
 	}
-	// at this point we didn't find the symbol
 	return NULL;
 }
 
-// Checks if a symbol was already declared in the current scope
+/* Checks if a symbol was already declared in the current scope */
 bool conflictingSymbol(char *name, int kind)
 {
 	int i;
+
 	// Switch for checking if it's a const, var, or proc
 	switch (kind)
 	{
@@ -409,14 +638,14 @@ bool conflictingSymbol(char *name, int kind)
 		// Run through the table
 		for (i = sym_index - 1; i != -1; i--)
 		{
-			// Only entries on our current level cause conflicts
-			if (table[i].level != curLevel)
+			// Only entries on our current level are valid
+			if (table[i].level != level)
 				break;
 
 			// If it has the same name and kind == 1 or 2
 			if (strcmp(table[i].name, name) == 0 && kind == 1)
 				// Then yes, it conflicts
-				return true;
+				return 1;
 			else if (strcmp(table[i].name, name) == 0 && kind == 2)
 				// Then yes, it conflicts
 				return true;
@@ -427,8 +656,8 @@ bool conflictingSymbol(char *name, int kind)
 		// Run through the table
 		for (i = sym_index - 1; i != -1; i--)
 		{
-			// Only entries on our current level cause conflicts
-			if (table[i].level != curLevel)
+			// Only entries on our current level are valid
+			if (table[i].level != level)
 				break;
 
 			// If it has the same name and kind == 3
@@ -442,23 +671,29 @@ bool conflictingSymbol(char *name, int kind)
 		break;
 	}
 
-	// If it reaches here, it never conflicted, so return false (0)
+	// If it reaches here, it never conflicted, so return false
 	return false;
 }
 
-// Marks symbols in the current scope before we exit it
+/* Marks symbols in the current scope before we exit it */
 void markSymbolsInScope()
 {
 	for (int i = sym_index - 1; i != -1; i--)
 	{
-		if (table[i].mark == 1) // should only be true when marking main
+		// Should only be true when marking main
+		if (table[i].mark == 1)
 			continue;
-		if (table[i].level != curLevel) // done marking the current scope
+
+		// If it's done marking the current scope, break
+		if (table[i].level != level)
 			break;
+		
+		// Mark the current symbol
 		table[i].mark = 1;
 	}
 }
 
+/* Print the error thrown */
 void printErrorMessage(int x)
 {
 	const char *errors[] =
@@ -484,6 +719,7 @@ void printErrorMessage(int x)
 		printf("Parser Error: %s\n", errors[x]);
 }
 
+/* Print the Symbol Table */
 void printtable()
 {
 	int i;
